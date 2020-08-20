b13127bafa8cb062c3e19f7566cdbb5d2fb188cf
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-08-20 13:47:09 -0400

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+public class Triangle {
+
+        public static void main(String[] args) throws Exception {
+            //write your code here
+            Scanner sc = new Scanner (System.in);
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a<b+c && b<a+c && c<a+b)
+            {
+                System.out.println("The triangle is possible.");
+
+            }
+            else
+                System.out.println("The triangle is not possible.");
+
+        }
+
+
+
+}
