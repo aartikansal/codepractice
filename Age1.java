bbd63d59eac9df3760f0e364acc5d823dab1956e
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-08-29 23:04:20 -0400

    Add files via upload

---
+++ Age1.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.io.*;
+public class Age1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int c = Integer.parseInt(b);
+
+        if (c > 20) {
+            System.out.println("18 is old enough");
+        }
+    }
+}
