65428d670a2fd66d116b3eed315f04dd771b60f3
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-02 22:01:26 -0400

    Add files via upload

---
+++ ForEven.java
@@ -0,0 +1,13 @@
+package com.company;
+public class ForEven {
+public static void main(String[] args) throws Exception {
+        //write your code here
+
+        for(int i =2; i<=100; i=i+2)
+        {
+        System.out.println(i);
+        }
+
+        }
+
+}
