dfb533109ab8f39ceb0cff3e80c65c50b1ee4ef5
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-02 22:40:31 -0400

    Add files via upload

---
+++ Triangle8.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Triangle8 {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}
