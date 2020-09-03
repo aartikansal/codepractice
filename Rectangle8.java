289347da2568d4c559c11d68d675a22b0376de47
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-02 22:25:26 -0400

    Add files via upload

---
+++ Rectangle8.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Rectangle8 {
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        for(int i=1; i<=m; i++)
+     {
+        for(int j =1; j<=n; j++)
+        {
+            System.out.print(8);
+        }
+        System.out.println("");
+     }
+                                                           }
+                      }
