771973c5ebd3990a7420fda2ebcfa50b20e2fe3d
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-03 13:14:31 -0400

    Add files via upload

---
+++ LinesPattern.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class LinesPattern {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+        for(int i = 1;i<10;i++)
+        {
+            System.out.print("8"+" ");
+        }
+        for (int j=0;j<11;j++)
+        {
+            System.out.println("8");
+        }
+        }
+    }
