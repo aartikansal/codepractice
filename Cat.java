88b26fee1a5ab029f85147c0b8cea51847be0ba5
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-04 23:39:40 -0400

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //write your code here
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        cat1.age = 3;
+        cat2.age = 5;
+        cat1.weight = 16;
+        cat2.weight = 14;
+        cat1.strength = 7;
+        cat2.strength = 6;
+
+        if (cat1.fight(cat2)) {
+            return true;
+
+        }
+
+
+        public static void main (String[]args){
+
+        }
+
+    }
+}
