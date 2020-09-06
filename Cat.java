87155b2227ab518d707d6e0105af3a6b6c368b2f
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-05 21:51:02 -0400

    Add files via upload

--- Cat.java
+++ Cat.java
@@ -11,24 +11,24 @@ public class Cat {
 
     public boolean fight(Cat anotherCat) {
         //write your code here
+        if((this.strength>anotherCat.strength)&&(this.weight>anotherCat.weight)&&(this.age>anotherCat.age)){
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
         Cat cat1 = new Cat();
         Cat cat2 = new Cat();
-        cat1.age = 3;
-        cat2.age = 5;
-        cat1.weight = 16;
-        cat2.weight = 14;
-        cat1.strength = 7;
-        cat2.strength = 6;
-
-        if (cat1.fight(cat2)) {
-            return true;
-
-        }
-
-
-        public static void main (String[]args){
-
-        }
-
+        cat1.strength=20;
+        cat1.age=5;
+        cat1.weight=20;
+        cat2.age=10;
+        cat2.weight=15;
+        cat2.strength=13;
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat2.fight(cat1));
     }
 }
