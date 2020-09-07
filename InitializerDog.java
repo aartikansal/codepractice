cc0e6dac2ff1a64fde7a3de23bb1474b60a569ca
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-06 20:52:09 -0400

    Add files via upload

---
+++ InitializerDog.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class InitializerDog {
+    private String name;
+    private int height;
+    private String color;
+
+    public void initialize(String name)
+    {
+        this.name = name;
+    }
+    public void initialize(String name, int height)
+    {
+        this.name = name;
+        this.height = height;
+    }
+    public void initialize(String name, int height, String color)
+
+    {
+        this.name= name;
+        this.height = height;
+        this.color = color;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
