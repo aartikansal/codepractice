859a846f49cbcae353345db96f93ef7dd8bed0e4
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-07 18:02:40 -0400

    Add files via upload

---
+++ C_Circle.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class C_Circle {
+
+        public double x;
+        public double y;
+        public double radius;
+
+        public C_Circle()
+        {
+
+        }
+        public C_Circle(double radius, double x, double y)
+        {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+
+        }
+        public C_Circle(double radius, double x)
+        {
+            this.radius = radius;
+            this.x = x;
+        }
+        public C_Circle(double y)
+        {
+            this.y = y;
+        }
+        public static void main(String[] args) {
+
+        }
+    }
