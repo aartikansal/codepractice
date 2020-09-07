9cc3b042c2951991e6de84e319b3200e82394b67
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-07 10:53:08 -0400

    Add files via upload

---
+++ Friend_constructor.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Friend_constructor {
+        String name;
+        int age;
+        char sex;
+        public  Friend_constructor(String name)
+        {
+            this.name = name;
+        }
+        public Friend_constructor(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Friend_constructor(String name, int age, char sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+        public static void main(String[] args) {
+
+        }
+    }
+
