2e5aa2dfcf7484b677692d5b58dfc1db873e00fc
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-07 14:46:29 -0400

    Add files via upload

---
+++ Wheel.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+public class Wheel {
+    public double x;
+    public double y;
+    public double r;
+
+    public Wheel(double x, double y, double r)
+    {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
