edc14c1e468f14285ef48f18c7c150be95686d2b
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-08 15:20:25 -0400

    Add files via upload

---
+++ Max_N.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+import java.io.*;
+
+
+
+        public class Max_N {
+            public static void main(String[] args) throws Exception {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                int maximum = 0;
+                int N = Integer.parseInt(reader.readLine());//reads a number N
+                if (N > 0) {
+                    maximum = Integer.parseInt(reader.readLine());
+                    for (int i = 1; i < N; i++) {   //length is N
+                        maximum = Math.max(maximum, Integer.parseInt(reader.readLine()));
+                    }
+                    System.out.println(maximum);
+                }
+            }
+        }
+
