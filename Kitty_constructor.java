e24aeaab8288fa9684a207f612da9e15a8fc763d
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-08 14:21:05 -0400

    Add files via upload

---
+++ Kitty_constructor.java
@@ -0,0 +1,48 @@
+package com.company;
+
+
+
+        public class Kitty_constructor {
+        public static void main(String[] args) {
+            Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+            Dog   snoopyDog = new Dog("Snoopy",8,4);
+            Cat   kittyCat = new Cat("Kitty",5,4);
+        }
+
+        public static class Mouse {
+            String name;
+            int height;
+            int tail;
+
+            public Mouse(String name, int height, int tail) {
+                this.name = name;
+                this.height = height;
+                this.tail = tail;
+            }
+        }
+
+        public static class Dog{
+            String name;
+            int height;
+            int tail;
+            public Dog(String name, int height, int tail) {
+                this.name = name;
+                this.height = height;
+                this.tail = tail;
+            }
+
+        }
+        public static class Cat{
+            String name;
+            int height;
+            int tail;
+            public Cat(String name, int height, int tail) {
+                this.name = name;
+                this.height = height;
+                this.tail = tail;
+            }
+
+        }
+
+    }
+
