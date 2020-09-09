4141d91fdf95fef855f2c665b26240bec08aa934
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-09 11:27:33 -0400

    Add files via upload

---
+++ Static_Cat.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Static_Cat {
+        static int catCount;
+        public Static_Cat()
+        {
+            this.catCount +=1;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+
