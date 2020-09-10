94ba8beaee93683edd628ef7a616653cde6d0929
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-10 12:57:51 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+package com.company;
+
+
+
+
+
+
+    public class Calculator {
+        public static int plus(int a, int b) {
+            int sum = a+b;
+            return sum;
+        }
+
+        public static int minus(int a, int b) {
+            int difference = a-b;
+            return difference;
+        }
+
+        public static int multiply(int a, int b) {
+            int product = a*b;
+            return product;
+        }
+
+        public static double divide(int a, int b) {
+            double c = (double) a/b;
+            return c;
+        }
+
+        public static double percent(int a, int b) {
+            double c = (double) a/b;
+            return ((double)b/100)*a;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
