258618e5f7b7d2de5be3f18ce24e94036e523c17
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-11 13:41:34 -0400

    Add files via upload

---
+++ More_Static.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class More_Static {
+
+        public static String name;
+        public static class Cat {
+            public static String name;
+        }
+
+        public static class Dog {
+            public static String name;
+
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+
