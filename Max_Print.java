f788d4aec1a1fab3c45aa5e3f3ed8bacac3c6488
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-11 14:01:18 -0400

    Add files via upload

---
+++ Max_Print.java
@@ -0,0 +1,24 @@
+package com.company;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+        public class Max_Print {
+            public static int max = 100;
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String max = "The max is ";
+            int a = Integer.parseInt(reader.readLine());
+            int b = Integer.parseInt(reader.readLine());
+            max += a > b ? a : b;
+
+            System.out.println(max);
+        }
+
+    }
+
