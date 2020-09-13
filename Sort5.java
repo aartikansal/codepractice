afb9ecfaba0a7aedd3a7d013cc43417c6e53b1a5
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-12 21:00:10 -0400

    Add files via upload

--- Sort5.java
+++ Sort5.java
@@ -1,41 +1,22 @@
 package com.company;
-
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-
-
+import java.util.Scanner;
+import java.util.Arrays;
 
 
 /*
 Ascending numbers
-
 */
-
-
-public class Sort5 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Integer[] numbers  = new Integer[5];
-        int j = 0;
-
-
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(reader.readLine());
+     public class Sort5 {
+     public static void main(String[] args) throws Exception {
+        int[] myList = new int[5];
+        Scanner kb = new Scanner(System.in);
+        for (int i = 0; i < myList.length; i++) {
+            myList[i] = kb.nextInt();
         }
-
-        for (Object number : ascending(numbers)){
-            j++;
-            System.out.println((int)number);
+        for (int i = 0; i < myList.length; i++) {
+            Arrays.sort(myList);
+            System.out.println(myList[i]);
         }
-    }
-
-    public static Collection ascending(Integer[] list){
-
-        Collection integers = new TreeSet<Integer>();
-        Collections.addAll(integers, list);
-
-        return integers;
     }
 }
+
