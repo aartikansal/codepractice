e773b6e6043ae642d6d1a3c574cca6f870e4cdf7
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-13 21:16:18 -0400

    Add files via upload

--- House_Array.java
+++ House_Array.java
@@ -4,23 +4,27 @@ import java.io.BufferedReader;
 import java.io.*;
 public class House_Array {
     public static void main(String[] args) throws Exception {
-        //write your code here
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[15];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(br.readLine());
-            int sumEven = 0;
-            int sumOdd = 0;
-            if (i % 2 == 0) {
-                sumEven = sumEven + array[i];
-                System.out.println("Odd-numbered houses have more residents.");
-            }
-            else
-                sumOdd = sumOdd + array[i];
-            System.out.println("Even-numbered houses have more residents.");
-
-        }
+    int[] array = new int[15];
+    int sum1=0;
+    int sum2=0;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0;i<15;i++){
+        array[i] = Integer.parseInt(reader.readLine());
 
     }
+        for(int i=0;i<15;i++){
+        sum1 = sum1 + array[i];
+        i++;
+    }
 
+        for(int i=1;i<15;i++){
+        sum2 += array[i];
+        i++;
+    }
+
+        if(sum1>sum2){
+        System.out.println("Even-numbered houses have more residents.");
+    }else
+            System.out.println("Odd-numbered houses have more residents.");
+}
 }
