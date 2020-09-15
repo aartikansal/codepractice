d62d2aabf3d4615481721dae1ad54e931390c319
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-14 23:06:32 -0400

    Add files via upload

--- House_Array.java
+++ House_Array.java
@@ -10,14 +10,17 @@ public class House_Array {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for(int i=0;i<15;i++){
         array[i] = Integer.parseInt(reader.readLine());
-
+    // Scanner class can also be used
     }
+        // either the actual size or length.array
         for(int i=0;i<15;i++){
+        // sum1 is for even
         sum1 = sum1 + array[i];
         i++;
     }
 
         for(int i=1;i<15;i++){
+        // sum2 is for odd
         sum2 += array[i];
         i++;
     }
