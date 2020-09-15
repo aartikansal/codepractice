79f774d4156181501f9165570bd21cef65ce5aa2
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-14 23:46:41 -0400

    Add files via upload

--- DollarPattern.java
+++ DollarPattern.java
@@ -1,19 +1,39 @@
 package com.company;
-import java.io.*;
-    public class DollarPattern {
+
+public class DollarPattern {
     public static void main(String[] args) throws Exception {
 
 
-        int i=1;
-        int j=1;
-        while(i<=10){
-            while(j<=10) {
+        int i = 1;
+        int j = 1;
+        while (i <= 10) {
+            while (j <= 10) {
                 System.out.print("$");
                 j++;
             }
             System.out.println(" ");
-            j=1;
+            j = 1;
             i++;
         }
     }
 }
+         //alternate for loop
+        /*for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("$");
+            }
+            System.out.println(" ");
+        }
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
