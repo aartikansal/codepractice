792969dc6d2e5393c4f894e7ba6b4d0244db3e90
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-18 13:18:43 -0400

    Add files via upload

--- AddHam_List.java
+++ AddHam_List.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 
 public class AddHam_List {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add(2, "Ham");
-        list.add(1, "Ham");
-        list.add(0, "Ham");
+        ArrayList<String > list = new ArrayList<>();
+        list.add("Sam");
+        list.add("I");
+        list.add("Am");
+        String H = "Ham";
 
-        for (String s : list) {
-            System.out.println(s);
+        for (int i = 1; i < list.size()+1; i += 2) {
+            list.add(i, H);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
     }
 }
+
+
+
+
 
