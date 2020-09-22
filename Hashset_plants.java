73fe5a811b3cf50ab6a2c2c6e8e165d90df4103a
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-22 13:39:31 -0400

    Add files via upload

---
+++ Hashset_plants.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+HashSet of plants
+
+*/
+public class Hashset_plants {
+        public static void main(String[] args) throws Exception {
+            Set<String> set = new HashSet<String>();
+            set.add("watermelon");
+            set.add("banana");
+            set.add("cherry");
+            set.add("pear");
+            set.add("cantaloupe");
+            set.add("blackberry");
+            set.add("ginseng");
+            set.add("strawberry");
+            set.add("iris");
+            set.add("potato");
+
+            Iterator<String> iterator= set.iterator();
+
+            while(iterator.hasNext())
+            {
+                String text = iterator.next();
+                System.out.println(text);
+            }
+
+        }
+    }
+
