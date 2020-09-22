9ef501eb55e24aab5e25768a88b4234f25d833f6
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-22 13:54:12 -0400

    Add files via upload

---
+++ Hashmap_10pairs.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+
+
+/*
+HashMap of 10 pairs
+
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmap_10pairs {
+        public static void main(String[] args) throws Exception {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("watermelon", "melon");
+            map.put("banana", "fruit");
+            map.put("cherry", "fruit");
+            map.put("pear", "fruit");
+            map.put("cantaloupe", "melon");
+            map.put("blackberry", "fruit");
+            map.put("ginseng", "root");
+            map.put("strawberry", "fruit");
+            map.put("iris", "flower");
+            map.put("potato", "tuber");
+
+        }
+    }
+
