e641a59dad901d90a9e9417275901973bb006890
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-24 08:09:19 -0400

    Add files via upload

--- ArithmeticMean.java
+++ ArithmeticMean.java
@@ -5,7 +5,6 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
     public class ArithmeticMean {
         public static void main(String[] args) throws Exception {
             Scanner sc = new Scanner(System.in);
