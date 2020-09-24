1f2ae71ca871b814a0fa1fa64bf949b4e0ed1444
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-23 21:52:20 -0400

    Add files via upload

--- Cats.java
+++ Cats.java
@@ -3,7 +3,6 @@ package com.company;
 public class Cats {
 
     public static void main(String[] args) {
-        //write your code here
         Cat cat1=  new Cat("Rosie",5,14, 6);
         Cat cat2 = new Cat("Tyler", 6, 17, 7);
         Cat cat3 = new Cat("Ray",3, 15, 8);
