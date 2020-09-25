c23db88d541434769a57ca406b5b078424a4761c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-25 14:53:00 -0400

    Add files via upload

---
+++ Linked_Array.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.*;
+/*
+LinkedList and ArrayList
+*/
+
+public class Linked_Array {
+    public static Object createArrayList() {
+        ArrayList<String> list = new ArrayList<String>();
+        return list;
+
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+        LinkedList<String>  Llist = new LinkedList<String>();
+        return Llist;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
