d3a53d72343205d295e02f8581b4f3e871a7c1c0
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-26 12:53:39 -0400

    Add files via upload

---
+++ SuitableList_Task_AL.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+/*
+Method quartet
+
+*/
+
+public class SuitableList_Task_AL {
+    public static List getListForGet() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        return list;
+
+    }
+
+    public static List getListForSet() {
+        ArrayList<Integer>  list = new ArrayList<Integer>();
+        return list;
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList<Integer> list1 = new LinkedList<Integer>();
+        return list1;
+    }
+
+    public static List getListForRemove() {
+        LinkedList<Integer>  list1 = new LinkedList<Integer>();
+        return list1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
