80eb8eb5282601ac9fe21ff1f0f537594bdc33dc
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-09-27 20:17:53 -0400

    Add files via upload

---
+++ Remove_500_Map.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Only for the rich
+
+*/
+
+public class Remove_500_Map {
+    public static HashMap<String, Integer> createMap() {
+        //write your code here
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Kansal", 1000);
+        map.put("Dhingra", 700);
+        map.put("Raut", 400);
+        map.put("Hava", 350);
+        map.put("Galo", 900);
+        map.put("Kans", 1000);
+        map.put("Dhing", 700);
+        map.put("Rau", 400);
+        map.put("Hav", 350);
+        map.put("Gal", 900);
+        return map;
+    }
+    public static void removeItemFromMap(HashMap<String, Integer> map) {
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            if (pair.getValue() < 500) {
+                map.remove(pair.getKey());
+                removeItemFromMap(map);
+                break;
+            }
+
+        }
+    }
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
