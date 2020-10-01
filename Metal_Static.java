1cadba9243222a74716f0e4d83d942365a1a9cb6
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-01 13:03:57 -0400

    Add files via upload

--- Metal_Static.java
+++ Metal_Static.java
@@ -1,13 +1,11 @@
 package com.company;
 
 public class Metal_Static {
-
         public static class KissMyShinyMetalRearActuator {
             String name;
         }
         public static void main(String[] args) {
             KissMyShinyMetalRearActuator metal = new KissMyShinyMetalRearActuator();
-
             System.out.println(metal);
         }
     }
