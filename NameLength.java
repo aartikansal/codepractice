50ea29b7950a69725f4c417ec00fa2116111985e
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-02 19:31:35 -0400

    Add files via upload

--- NameLength.java
+++ NameLength.java
@@ -1,17 +1,13 @@
 package com.company;
 import java.io.*;
 
-
-
-    public class NameLength{
+        public class NameLength{
         public static void main(String[] args) throws Exception {
-            //write your code here
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String name1 = reader.readLine();
             String name2 = reader.readLine();
             int x = name1.length();
             int y = name2.length();
-
             if(name1.equals(name2)){
                 System.out.println("The names are identical");
             }
