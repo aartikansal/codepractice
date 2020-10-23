c1505a8d6758d27905a603c3a40096bd6f0ea2ca
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-22 22:14:40 -0400

    Add files via upload

---
+++ StackTrace.java
@@ -0,0 +1,42 @@
+package com.company;
+
+/*
+Who called me?
+
+*/
+
+public class StackTrace {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method2() {
+        method3();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method5() {
+        System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+        return  Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+}
+
