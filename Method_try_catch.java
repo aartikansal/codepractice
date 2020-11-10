f1805347230b73fe91037e025913e6537f606d07
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-10 17:18:10 -0500

    Add files via upload

--- Method_try_catch.java
+++ Method_try_catch.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Method_try_catch {
-
     public static void main(String[] args) {
         readData();
     }
@@ -13,19 +12,17 @@ public class Method_try_catch {
     public static void readData() {
 
         ArrayList<Integer> list = new ArrayList<>();
-        try{
+        try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             int x;
-            while (true){
-                x=Integer.parseInt(reader.readLine());
+            while (true) {
+                x = Integer.parseInt(reader.readLine());
                 list.add(x);
-                System.out.println(list);
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
             }
         }
-        catch (Exception e){
-            System.out.println(e);
-        }
-        //write your code here
     }
 }
-
