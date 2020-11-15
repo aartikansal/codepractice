f1f07d139c4239266c25d28d6220bd7a678d9baf
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-15 17:50:49 -0500

    Add files via upload

--- Static_modifier.java
+++ Static_modifier.java
@@ -1,28 +1,22 @@
 package com.company;
 
-
-
-
-
 /*
 Static modifiers are out of place
-
 */
 public class Static_modifier {
-    public static int a = 5;
-    public static int B = 2 * a;
-    public int C = a * B;
-    public static int D = a * B;
+    public  static int A = 5;
+    public  static int B = 2 * A;
+    public int C = A * B;
+    public  static int D = A * B;
 
     public static void main(String[] args) {
-        Solution room = new Solution();
-        room.a = 5;
-
-        Solution.d = 5;
+        Static_modifier room = new Static_modifier();
+        room.A = 5;
+        Static_modifier.D = 5;
     }
 
-    public int geta() {
-        return a ;
+    public int getA() {
+        return A;
     }
 
 }
