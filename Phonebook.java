66b53b9ee4e0622811c9990dea04af5cec801bad
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-22 14:55:55 -0500

    Add files via upload

---
+++ Phonebook.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Phonebook {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Map<String, String> phoneBook = new HashMap<String, String>();
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            String phone = in.nextLine();
+            phoneBook.put(name, phone);
+        }
+        while (in.hasNextLine()) {
+            String name = in.nextLine();
+            if (phoneBook.containsKey(name)) {
+                System.out.println(name + "=" + phoneBook.get(name));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
