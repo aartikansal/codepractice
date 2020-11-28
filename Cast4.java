06c2685544ac2d8b50db5ccd2b5675eea88ecd3c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-28 18:24:42 -0500

    Add files via upload

---
+++ Cast4.java
@@ -0,0 +1,14 @@
+package com.company;
+/*
+Task No. 4 about integer type conversions
+
+*/
+
+public class Cast4 {
+    public static void main(String[] args) {
+        short number = 9;
+        char zero = '0';
+        int nine =  (zero + number);
+        System.out.println((char)nine);
+    }
+}
