4e4bca3a9d11f48e4f73a2a0051d6498fc5e09ac
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-29 23:37:49 -0500

    Add files via upload

--- Cast5.java
+++ Cast5.java
@@ -6,7 +6,7 @@ public class Cast5 {
     public static void main(String[] args) {
         int a =  44;
         int b =  300;
-        int c =  (b - a);
+        short c = (short) (b - a);
         System.out.println(c);
     }
 }
