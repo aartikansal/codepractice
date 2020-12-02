364b54652d188520dda970f7252b4f891c067cc4
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-02 16:54:20 -0500

    Add files via upload

---
+++ String_listNumbers.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Number of letters
+*/
+
+public class String_listNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Alphabet
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        char[] abcArray = abc.toCharArray();
+
+        ArrayList<Character> alphabet = new ArrayList<>();
+        for (char letter : abcArray) {
+            alphabet.add(letter);
+        }
+
+        // Read in strings
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+
+
+        // write your code here
+    }
+
+}
